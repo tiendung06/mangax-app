42b1861e6e40a7fc84daddf841a1514a0abde948
Do Tien Dung <dungdotien14@gmail.com>
2023-01-19 13:52:00 +0700

    Config project

---
+++ src/constants/api.ts
@@ -0,0 +1,46 @@
+const domain = "https://api.jikan.moe/v4";
+const anime = `${domain}/anime`;
+const manga = `${domain}/manga`;
+const characters = `${domain}/characters`;
+const recommendations = `${domain}/recommendations`;
+const season = `${domain}/season`;
+const top = `${domain}/top`;
+
+const LIMIT = 20;
+
+export const jikanAPI = {
+  //Anime
+  getListAnime: (page = 1) => `${anime}?page=${page}&limit=${LIMIT}`,
+  getAnimeSearch: (filter: string) => `${anime}?q=${filter}$limit=${LIMIT}`,
+  getAnimeById: (id: number) => `${anime}/${id}`,
+  getAnimeCharacters: (id: number) => `${anime}/${id}/characters`,
+  getAnimeVideos: (id: number) => `${anime}/${id}/videos`,
+  getAnimeEpisodes: (id: number) => `${anime}/${id}/episodes`,
+  getAnimeVideosEpisodes: (id: number) => `${anime}/${id}/videos/episodes`,
+  getAnimePictures: (id: number) => `${anime}/${id}/pictures`,
+  getAnimeRecommendations: (id: number) => `${anime}/${id}/recommendations`,
+  getAnimeReviews: (id: number) => `${anime}/${id}/reviews`,
+  //Manga
+  getListManga: (page = 1) => `${manga}?page=${page}&limit=${LIMIT}`,
+  getMangaSearch: (filter: string) => `${manga}?q=${filter}$limit=${LIMIT}`,
+  getMangaById: (id: number) => `${manga}/${id}`,
+  getMangaCharacters: (id: number) => `${manga}/${id}/characters`,
+  getMangaPictures: (id: number) => `${manga}/${id}/pictures`,
+  getMangaRecommendations: (id: number) => `${manga}/${id}/recommendations`,
+  getMangaReviews: (id: number) => `${manga}/${id}/reviews`,
+  //Characters
+  getListCharacters: (page = 1) => `${characters}?page=${page}`,
+  getCharacterById: (id: number) => `${characters}/${id}`,
+  getCharacterVoiceActors: (id: number) => `${characters}/${id}/voices`,
+  getCharacterPictures: (id: number) => `${characters}/${id}/pictures`,
+  //Recommendations
+  getRecentAnimeRecommendations: (page = 1) =>
+    `${recommendations}/anime?page=${page}`,
+  getRecentMangaRecommendations: (page = 1) =>
+    `${recommendations}/manga?page=${page}`,
+  //Season
+  getSeasonNow: (page = 1) => `${season}/now?page=${page}`,
+  //Top
+  getTopAnime: (page = 1) => `${top}/anime?page=${page}&limit=${LIMIT}`,
+  getTopManga: (page = 1) => `${top}/manga?page=${page}$limit=${LIMIT}`,
+};
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ["Inter", "sans-serif"],
+      },
+      colors: {
+        primary: "#ED1D29",
+        secondary: "#6F49FD",
+        "icon-color": "#A2A2A8",
+        white: "#FFFFFF",
+        whiteSoft: "#FCFBFF",
+        graySoft: "#FCFCFC",
+        grayf3: "#f3f3f3",
+        strock: "#F1F1F3",
+        lite: "#FCFCFD",
+        error: "#EB5757",
+        darkbg: "#13131A",
+        darkSecondary: "#1C1C24",
+        softDark: "#22222C",
+        darkSoft: "#24242C",
+        darkStroke: "#3A3A43",
+        darkRed: "#422C32",
+      },
+    },
+  },
+  plugins: [],
+};
